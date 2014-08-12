5721d4b9980ca142faa1e34d10ba3c766e2c6bb2
takatoh <takatoh.m@gmail.com>
2014-08-12 22:51:29 +0900

    Be able to post a photo.

--- lib/sbr/post_photo_command.rb
+++ lib/sbr/post_photo_command.rb
@@ -12,15 +12,17 @@ module Sbr
 
     def initialize
       @options = {
-        :source   => "",
-        :page_url => "",
-        :tags     => ""
+        :repository => "",
+        :source     => "",
+        :page_url   => "",
+        :tags       => ""
       }
       @parser = OptionParser.new
       @parser.banner =<<EOB
   #{@parser.program_name} post - Post photo(s).
   Usage: #{@parser.program_name} post [options] <photofile>
 EOB
+      @parser.on('-R', '--repository=URL', 'Set repository url.'){|v| @options[:repository] = v}
       @parser.on('-s', '--source=SOURCE', 'Set source of photo.'){|v| @options[:source] = v}
       @parser.on('-p', '--page_url=URL', 'Set webpage url.'){|v| @options[:page_url] = v}
       @parser.on('-t', '--tags=TAGS', 'Set tags.'){|v| @options[:tags] = v}
@@ -29,13 +31,15 @@ EOB
     def exec(argv)
       photofile = argv.shift
       hc = HTTPClient.new
-      post_data = {
-        "url" => @options[:source],
-        "page_url" => @options[:page_url],
-        "tags"     => @options[:tags],
-        "file"     => File.open(photofile, "br")
-      }
-      hc.post(post_data)
+      File.open(photofile, "rb") do |file|
+        post_data = {
+          "url" => @options[:source],
+          "page_url" => @options[:page_url],
+          "tags"     => @options[:tags],
+          "file"     => file
+        }
+        res = hc.post(@options[:repository] + "post", post_data)
+      end
     end
 
   end
