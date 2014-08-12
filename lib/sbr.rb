970bd871f6c4d91fd957ad5d4222a9d122422025
takatoh <takatoh.m@gmail.com>
2014-08-12 22:38:23 +0900

    lib/sbr.rb: require 'sbr/subcommand'.

--- lib/sbr.rb
+++ lib/sbr.rb
@@ -1,17 +1,7 @@
 require "sbr/version"
+require "sbr/subcommand"
 require "sbr/post_photo_command"
 
 
 module Sbr
-
-  class Subcommand
-    def parse(argv)
-      @parser.parse! argv
-    end
-
-    def help
-      @parser.help
-    end
-  end
-
 end
