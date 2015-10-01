542975df0bdd1f1f0e8a1a761599b868b12e0fbb
takatoh <takatoh.m@gmail.com>
2015-10-01 20:40:06 +0900

    lib/sbr/post_photo_command.rb: Move private methods into Sbr::Subcommand class.

--- lib/sbr/subcommand.rb
+++ lib/sbr/subcommand.rb
@@ -1,6 +1,7 @@
 module Sbr
 
   class Subcommand
+
     def parse(argv)
       @parser.parse! argv
     end
@@ -8,6 +9,48 @@ module Sbr
     def help
       @parser.help
     end
+
+    private
+
+    def post_photo(photofile, opts = {})
+      puts photofile
+      File.open(photofile, "rb") do |file|
+        post_data = {
+          "url"      => opts["url"]      || @options[:source],
+          "page_url" => opts["page_url"] || @options[:page_url],
+          "tags"     => opts["tags"]     || @options[:tags],
+          "file"     => file
+        }
+        post_data["add_tags"] = true if opts["add_tags"] || @options[:add_tags]
+        post_url = @options[:repository] + "api/post"
+        res = @hc.post(post_url, post_data)
+        result = JSON.parse(res.body)
+        if result["status"] == "Accepted"
+          photo = result["photo"]
+          puts "  => Accepted: id=#{photo['id']} size=#{photo['width']}x#{photo['height']}"
+          @counter[:accepted] += 1
+        elsif result["status"] == "Add tags"
+          photo = result["photo"]
+          puts "  => Added tags: #{photo["addedTags"].join(" ")} (id=#{photo['id']})"
+          @counter[:added_tags] += 1
+        else
+          case result["reason"]
+          when "Already exist"
+            photo = result["photo"]
+            puts "  => Rejected: Already exist(id=#{photo['id']})"
+            @counter[:rejected] += 1
+          when "Small photo"
+            puts "  => Rejected: Small photo"
+            @counter[:rejected] += 1
+          end
+        end
+      end
+    end
+
+    def photo?(file)
+      %w(.jpg .jpeg .png .bmp .gif).include?(File.extname(file))
+    end
+
   end
 
 end
