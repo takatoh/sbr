ad6cce1dffb176cf936e1a965847da1272de81ac
takatoh <takatoh.m@gmail.com>
2015-10-01 20:47:56 +0900

    lib/sbr/subcommand.rb: New private method: clip_photo.

--- lib/sbr/subcommand.rb
+++ lib/sbr/subcommand.rb
@@ -49,6 +49,38 @@ module Sbr
       end
     end
 
+    def clip_photo(photourl, opts = {})
+      puts photourl
+      post_data = {
+        "url"      => url,
+        "page_url" => opts["page_url"],
+        "tags"     => opts["tags"],
+        "force"    => opts["force"]
+      }
+      post_url = @options[:repository] + "api/clip"
+      res = @hc.post(post_url, post_data)
+      result = JSON.parse(res.body)
+      if result["status"] == "Accepted"
+        photo = result["photo"]
+        puts "  => Accepted: id=#{photo['id']} size=#{photo['width']}x#{photo['height']}"
+        @counter[:accepted] += 1
+      elsif result["status"] == "Add tags"
+        photo = result["photo"]
+        puts "  => Added tags: #{photo["addedTags"].join(" ")} (id=#{photo['id']})"
+        @counter[:added_tags] += 1
+      else
+        case result["reason"]
+        when "Already exist"
+          photo = result["photo"]
+          puts "  => Rejected: Already exist(id=#{photo['id']})"
+          @counter[:rejected] += 1
+        when "Small photo"
+          puts "  => Rejected: Small photo"
+          @counter[:rejected] += 1
+        end
+      end
+    end
+
     def photo?(file)
       %w(.jpg .jpeg .png .bmp .gif).include?(File.extname(file))
     end
