01365318dcaaf8acf79ab312243b54d130f51675
takatoh <takatoh.m@gmail.com>
2016-01-26 22:21:27 +0900

    lib/sbr/config_command.rb: New.

---
+++ lib/sbr/config_command.rb
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+
+
+require 'sbr/subcommand'
+require 'yaml'
+require 'optparse'
+
+
+module Sbr
+
+  class ConfigCommand < Subcommand
+
+    def initialize
+      @config_file = "#{ENV["HOME"]}/.sbrconfig.yml"
+      @config = YAML.load_file(@config_file)
+      @parser = OptionParser.new
+      @parser.banner =<<EOB
+  #{@parser.program_name} config - Set or get config.
+  Usage: #{@parser.program_name} config <key> [value]
+EOB
+    end
+
+    def exec(argv)
+      if argv.size == 1
+        puts @config[argv[0]]
+      elsif argv.size == 2
+        @config[argv[0]] = argv[1]
+        File.open(@config_file, "w") do |f|
+          f.print @config.to_yaml
+        end
+      end
+    end
+
+  end   # of class ConfigCommand
+
+end   # of module Sbr
