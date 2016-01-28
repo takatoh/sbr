c922659da7dca1e8358e5ddbfdfaa342fedae823
takatoh <takatoh.m@gmail.com>
2016-01-28 20:41:42 +0900

    lib/sbr/config_command.rb: --list option for sbr config command.

--- lib/sbr/config_command.rb
+++ lib/sbr/config_command.rb
@@ -11,15 +11,23 @@ module Sbr
 
     def initialize
       super
+      @options = {
+        :list => false
+      }
       @parser = OptionParser.new
       @parser.banner =<<EOB
   #{@parser.program_name} config - Set or get config.
   Usage: #{@parser.program_name} config <key> [value]
 EOB
+      @parser.on('-l', '--list', 'List configurations.'){|v| @options[:list] = true }
     end
 
     def exec(argv)
-      if argv.size == 1
+      if @options[:list]
+        @config.each do |k, v|
+          puts "#{k} = #{v}"
+        end
+      elsif argv.size == 1
         puts @config[argv[0]]
       elsif argv.size == 2
         @config[argv[0]] = argv[1]
