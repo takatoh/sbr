b9ae31f81ecc16b2eaecd3d34aadd183a875bba4
takatoh <takatoh.m@gmail.com>
2016-05-22 15:49:37 +0900

    Add subcommand: get-photo.

--- lib/sbr.rb
+++ lib/sbr.rb
@@ -4,7 +4,7 @@ require "sbr/post_photo_command"
 require "sbr/clip_photo_command"
 require "sbr/scrape_page_command"
 require "sbr/config_command"
-
+require "sbr/get_photo_command"
 
 module Sbr
 end
