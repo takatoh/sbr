cd4c33fd5383bfbde12d6a44a4a0d6db12e7acb8
takatoh <takatoh.m@gmail.com>
2017-08-23 21:31:01 +0900

    lib/sbr/get_photo_command.rb: --dump-json option.

--- lib/sbr/get_photo_command.rb
+++ lib/sbr/get_photo_command.rb
@@ -16,7 +16,8 @@ module Sbr
       @options = {
         :repository => @config["repository"],
         :limit      => nil,
-        :offset     => nil
+        :offset     => nil,
+        :json       => false
       }
       @parser = OptionParser.new
       @parser.banner =<<EOB
@@ -26,6 +27,7 @@ EOB
       @parser.on('-R', '--repository=URL', 'Set repository url.'){|v| @options[:repository] = v}
       @parser.on('-l', '--limit=N', 'Set limit to get.'){|v| @options[:limit] = v}
       @parser.on('-o', '--offset=O', 'Set offset to get.'){|v| @options[:offset] = v}
+      @parser.on('-j', '--json-dump', 'Dump JSON.'){|v| @options[:json] = true}
     end
 
     def exec(argv)
@@ -41,14 +43,18 @@ EOB
       end
       json = @hc.get(api_url).body
       photos = JSON.parse(json)
-      photos.each do |photo|
-        puts "Id:        #{photo['id']}"
-        puts "File name: #{photo['fileName']}"
-        puts "File size: #{photo['fileSize']}"
-        puts "MD5:       #{photo['md5']}"
-        puts "Size:      #{photo['width']}x#{photo['height']}"
-        puts "File URL:  #{photo['fileUrl']}"
-        puts ""
+      if @options[:json]
+        puts JSON.pretty_generate(photos)
+      else
+        photos.each do |photo|
+          puts "Id:        #{photo['id']}"
+          puts "File name: #{photo['fileName']}"
+          puts "File size: #{photo['fileSize']}"
+          puts "MD5:       #{photo['md5']}"
+          puts "Size:      #{photo['width']}x#{photo['height']}"
+          puts "File URL:  #{photo['fileUrl']}"
+          puts ""
+        end
       end
     end
 
