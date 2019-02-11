6faa52707b7fd7bbb9d434fc5c6334f9927229ce
takatoh <takatoh.m@gmail.com>
2019-02-11 10:45:14 +0900

    lib/sbr/get_photo_command.rb: --md5 option. Get photo by md5.

--- lib/sbr/get_photo_command.rb
+++ lib/sbr/get_photo_command.rb
@@ -17,29 +17,41 @@ module Sbr
         :repository => @config["repository"],
         :limit      => nil,
         :offset     => nil,
-        :json       => false
+        :json       => false,
+        :md5        => false
       }
       @parser = OptionParser.new
       @parser.banner =<<EOB
   #{@parser.program_name} get-photo - Get photo(s).
   Usage: #{@parser.program_name} get-photo [options] [id]
+         #{@parser.program_name} get-photo --md5 md5
 EOB
       @parser.on('-R', '--repository=URL', 'Set repository url.'){|v| @options[:repository] = v}
       @parser.on('-l', '--limit=N', 'Set limit to get.'){|v| @options[:limit] = v}
       @parser.on('-o', '--offset=O', 'Set offset to get.'){|v| @options[:offset] = v}
+      @parser.on('-m', '--md5', 'Get by md5.'){|v| @options[:md5] = true}
       @parser.on('-j', '--json-dump', 'Dump JSON.'){|v| @options[:json] = true}
     end
 
     def exec(argv)
       @hc = HTTPClient.new
-      unless argv.empty?
-        api_url = @options[:repository] + "api/photo/" + argv.first
+      if @options[:md5]
+        unless argv.empty?
+          api_url = @options[:repository] + "api/photo/md5/" + argv.shift
+        else
+          $stderr.puts "Error: Pleas specify MD5."
+          exit(1)
+        end
       else
-        api_url = @options[:repository] + "api/photos"
-        query = [:limit, :offset].map do |o|
-          @options[o] ? "#{o.to_s}=#{@options[o]}" : nil
-        end.compact.join("&")
-        api_url += "?#{query}" unless query.empty?
+        unless argv.empty?
+          api_url = @options[:repository] + "api/photo/" + argv.first
+        else
+          api_url = @options[:repository] + "api/photos"
+          query = [:limit, :offset].map do |o|
+            @options[o] ? "#{o.to_s}=#{@options[o]}" : nil
+          end.compact.join("&")
+          api_url += "?#{query}" unless query.empty?
+        end
       end
       json = @hc.get(api_url).body
       photos = JSON.parse(json)
