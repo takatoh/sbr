2e2b6502311dbf2d7e9d0416201f80e83a655824
takatoh <takatoh.m@gmail.com>
2024-01-06 16:21:02 +0900

    lib/sbr/subcommand.rb: Fix indent.

--- lib/sbr/subcommand.rb
+++ lib/sbr/subcommand.rb
@@ -36,35 +36,35 @@ module Sbr
 
     def post_photo(photofile, opts = {})
       puts photofile
-        post_data = {
-          "url"      => opts["url"]      || @options[:source],
-          "page_url" => opts["page_url"] || @options[:page_url],
-          "tags"     => opts["tags"]     || @options[:tags],
-          "file"     => HTTP::FormData::File.new(photofile)
-        }
-        post_data["add_tags"] = true if opts["add_tags"] || @options[:add_tags]
-        post_url = @options[:repository] + "api/post"
-        res = HTTP.post(post_url, :form => post_data)
-        result = JSON.parse(res.to_s)
-        if result["status"] == "Accepted"
+      post_data = {
+        "url"      => opts["url"]      || @options[:source],
+        "page_url" => opts["page_url"] || @options[:page_url],
+        "tags"     => opts["tags"]     || @options[:tags],
+        "file"     => HTTP::FormData::File.new(photofile)
+      }
+      post_data["add_tags"] = true if opts["add_tags"] || @options[:add_tags]
+      post_url = @options[:repository] + "api/post"
+      res = HTTP.post(post_url, :form => post_data)
+      result = JSON.parse(res.to_s)
+      if result["status"] == "Accepted"
+        photo = result["photo"]
+        puts "  => Accepted: id=#{photo['id']} size=#{photo['width']}x#{photo['height']}"
+        @counter[:accepted] += 1
+      elsif result["status"] == "Added tags"
+        photo = result["photo"]
+        puts "  => Added tags: #{photo["addedTags"].join(" ")} (id=#{photo['id']})"
+        @counter[:added_tags] += 1
+      else
+        case result["reason"]
+        when "Already exist"
           photo = result["photo"]
-          puts "  => Accepted: id=#{photo['id']} size=#{photo['width']}x#{photo['height']}"
-          @counter[:accepted] += 1
-        elsif result["status"] == "Added tags"
-          photo = result["photo"]
-          puts "  => Added tags: #{photo["addedTags"].join(" ")} (id=#{photo['id']})"
-          @counter[:added_tags] += 1
-        else
-          case result["reason"]
-          when "Already exist"
-            photo = result["photo"]
-            puts "  => Rejected: Already exist(id=#{photo['id']})"
-            @counter[:rejected] += 1
-          when "Small photo"
-            puts "  => Rejected: Small photo"
-            @counter[:rejected] += 1
-          end
+          puts "  => Rejected: Already exist(id=#{photo['id']})"
+          @counter[:rejected] += 1
+        when "Small photo"
+          puts "  => Rejected: Small photo"
+          @counter[:rejected] += 1
         end
+      end
     end
 
     def clip_photo(photourl, opts = {})
